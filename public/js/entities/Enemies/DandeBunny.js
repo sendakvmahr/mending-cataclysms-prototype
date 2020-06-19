3663996a760fa0ad31b0b7794c425e2ef4e9b76c
sendak <vivianmahr@gmail.com>
2020-06-19 16:05:01 -0700

    pages hosting config

--- public/js/entities/Enemies/DandeBunny.js
+++ public/js/entities/Enemies/DandeBunny.js
@@ -1,15 +1,15 @@
-define(["display/Animation", "entities/Entity", "physics/Vector", "lib/goody", "assets/vars"],
-function(Animation, Entity, Vector, goody, vars)
+define(["display/Animation", "entities/Enemy/Enemy", "physics/Vector", "lib/goody", "assets/vars"],
+function(Animation, Enemy, Vector, goody, vars)
 {    
-    Enemy.prototype = new Entity.Entity({"x":0, "y": 0});
-    Enemy.prototype.constructor = Enemy;
+    DandeBunny.prototype = new Enemy.Enemy({"x":0, "y": 0});
+    DandeBunny.prototype.constructor = DandeBunny;
 
-    function Enemy(info) {
+    function DandeBunny(info) {
         //location
         Entity.Entity.apply(this, arguments);
-        this.isEnemy = true;
+        this.isDandeBunny = true;
         // HERE FOR REFERENCE FOR LATER
-        this._sprite = new Animation.Animation(images.projectile_enemy, 1, 36, 72);
+        this._sprite = new Animation.Animation(images.projectile_DandeBunny, 1, 36, 72);
         //this._shadowSprite = new Animation.Animation(images.MCshadow, 1, 20, 8);
 
         this.rect.width = 21;
@@ -17,15 +17,15 @@ function(Animation, Entity, Vector, goody, vars)
         this._spriteOffset = new Vector.Vector(-8, -80);
     }
 
-    Enemy.prototype.applyAttack = function(attack) {
+    DandeBunny.prototype.applyAttack = function(attack) {
         this.toDelete = true;
     }
     
-    Enemy.prototype.update = function(input, map, collisionHandler, timeDelta) {
+    DandeBunny.prototype.update = function(input, map, collisionHandler, timeDelta) {
         // if it hasn't done anything for a while make a projection, otherwise it just sits there
     }
 
-    Enemy.prototype.drawImage = function(ctx, offset) {
+    DandeBunny.prototype.drawImage = function(ctx, offset) {
         var displayOffset = this.rect.position.add(offset);
         this._sprite.display(ctx, displayOffset.add(this._spriteOffset));
 
@@ -35,6 +35,6 @@ function(Animation, Entity, Vector, goody, vars)
     }
 
     return {
-        Enemy: Enemy
+        DandeBunny: DandeBunny
     };
 });
